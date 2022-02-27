b70bfb1e49793a0a032715eb7b433b19d910f135
Sachin Singh <49466686+thesachinsingh@users.noreply.github.com>
2022-02-27 23:39:41 +0530

    removed extra lines

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,28 +1,22 @@
 #insertion sort
+
 import random
-def insertionSort(A):
-    
+def insertionSort(A):    
     for i in range(1, len(A)): 
         temp = A[i] 
         flag = i-1
-
         for j in range(i-1, -1, -1):
-
             if temp<A[j]:           
                 A[j+1] = A[j]         
                 flag = j            
-            
             else:
                 break               
-
         A[flag] = temp  
-    
     print(f"Sorted Array {A}")
 
 
-
 def main():
-    X = [random.randint(-100,100) for i in range(10)]
+    X = [random.randint(0,100) for i in range(10)]
     print(X)
     insertionSort(X)
 

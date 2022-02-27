8fc171b06a6b53a9f7fba46ba934a9294c23f681
Sachin Singh <49466686+thesachinsingh@users.noreply.github.com>
2022-02-27 23:54:54 +0530

    fixed a logical error in line 7

--- insertion_sort.py
+++ insertion_sort.py
@@ -2,17 +2,20 @@
 
 import random
 def insertionSort(A):    
-    for i in range(1, len(A)): 
-        temp = A[i] 
-        flag = i-1
+    j = 0
+    for i in range(1, len(A)):
+        temp = A[i]
+        flag = i
         for j in range(i-1, -1, -1):
-            if temp<A[j]:           
-                A[j+1] = A[j]         
-                flag = j            
+            if temp<A[j]:
+                A[j+1] = A[j]
+                flag = j
             else:
-                break               
-        A[flag] = temp  
-    print(f"Sorted Array {A}")
+                break
+
+        A[flag] = temp 
+    
+    print(f'Sorted Array : {A}')
 
 
 def main():
